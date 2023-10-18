5f05ed0ce5f801cda3c68a070fce33f6a735a2fa
Ashwin1729 <mathematicswroteus@gmail.com>
2023-10-18 12:35:56 +0530

    updated api

--- middlewares/randomRequestMiddleware.js
+++ middlewares/randomRequestMiddleware.js
@@ -2,7 +2,8 @@
 
 const randomRequestMiddleware = (req, res, next) => {
   return res.status(404).json({
-    ERROR: "Invalid URL. Please refer API documention for all valid requests.",
+    ERROR:
+      "Invalid URL. Please refer to the API Documentation to know all the request endpoints!",
   });
 };
 
